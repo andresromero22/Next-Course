7694f62f40bd7d9d51aef00f0e819c908b405715
Andres Romero <andresfromero22@gmail.com>
2025-07-17 19:01:40 -0500

    authentication and autorization

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+
+export default async function Page() {
+  const totalCustomers = await fetchFilteredCustomers('')
+  return (
+    <main>
+      <CustomersTable customers={totalCustomers}/>
+    </main>
+  )
 }
