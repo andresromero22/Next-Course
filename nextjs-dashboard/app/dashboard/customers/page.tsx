6bc8872686b6ff72f67f326050718a42d05739d5
Andres Romero <andresfromero22@gmail.com>
2025-07-21 22:38:57 -0500

    final metadata

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,7 +1,10 @@
 import CustomersTable from "@/app/ui/customers/table";
 import { Suspense } from "react";
+import { Metadata } from "next";
 
-
+export const metadata: Metadata = {
+  title: "Customers"
+}
 export default async function Page(
   {
     searchParams : searchParamsPromise
